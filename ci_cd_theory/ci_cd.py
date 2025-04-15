6950b33773fed42aeb6d0f7a2a57d6b3d72c39a2
REYANSH M YADAV <mukesh.hansa4725@gmail.com>
2025-04-15 17:54:50 +0530

    ci_cd-theory_added

---
+++ ci_cd_theory/ci_cd.py
@@ -0,0 +1,18 @@
+# Folder====>
+#src folder===>
+# (1). __init__.py ### This is used for packages creation.
+# (2). main.py  ### This is the main entry point of the package
+
+# Tests folder ===>
+# (1). __init__.py  ### This is used for package creation
+#(2). test.py 
+
+# assert ==> check ===>  input to output
+
+# requirements.txt ===> numpy, pandas
+
+# Go to Extension ===> Install github actions
+
+# Readme.md ===> Message term
+
+# .github/workflows/app.yaml(yet another markup language) 
